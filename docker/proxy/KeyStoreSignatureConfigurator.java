ead7e37160d788cc8be91d9c16af319f1effee29
Randi Øyri <randi.oyri@digdir.no>
2024-07-04 13:30:25 +0200

    override whitelist of signatures

--- docker/proxy/KeyStoreSignatureConfigurator.java
+++ docker/proxy/KeyStoreSignatureConfigurator.java
@@ -20,6 +20,7 @@ package eu.eidas.auth.engine.configuration.dom;
 import static org.apache.commons.lang.StringUtils.trim;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.commons.lang.StringUtils;
+import java.util.regex.Pattern;
 
 import java.security.cert.X509Certificate;
 import java.util.ArrayList;
