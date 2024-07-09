a17568b7880571aac3198748df4451979f55c34c
Randi Øyri <randi.oyri@digdir.no>
2024-07-09 09:22:46 +0200

    ID-4524: Issuer frå vault og same namn på variabler som connector. Fikse java.security providers for systest. (#38)

--- docker/updateKeyStoreConfig.sh
+++ docker/updateKeyStoreConfig.sh
@@ -8,10 +8,10 @@ SIGNMODULE_SERVICE_FILE=/etc/config/eidas-proxy/SignModule_Service.xml
 if [ -f "$SIGNMODULE_SERVICE_FILE" ]; then
     echo "Update keystore-config in $SIGNMODULE_SERVICE_FILE" && printenv | grep PASSWORD
     sed -i "s/TRUSTSTORE_PASSWORD/$TRUSTSTORE_PASSWORD/g" $SIGNMODULE_SERVICE_FILE
-    sed -i "s/KEYSTORE_PASSWORD/$KEYSTORE_PASSWORD/g" $SIGNMODULE_SERVICE_FILE
-    sed -i "s/KEYSTORE_KEY_PASSWORD/$KEYSTORE_KEY_PASSWORD/g" $SIGNMODULE_SERVICE_FILE
-    echo "Remove hsm security provider from java"
-    sed 's/\SunPKCS11.*/SunPKCS11/g' /opt/java/openjdk/conf/security/java_bc.security > /opt/java/openjdk/conf/security/java_bc.security
+    sed -i "s/SIGN_KEYSTORE_PASSWORD/$SIGN_KEYSTORE_PASSWORD/g" $SIGNMODULE_SERVICE_FILE
+    sed -i "s/SIGN_KEYSTORE_KEY_PASSWORD/$SIGN_KEYSTORE_KEY_PASSWORD/g" $SIGNMODULE_SERVICE_FILE
+    cat /opt/java/openjdk/conf/security/plain.java_bc.security > /opt/java/openjdk/conf/security/java_bc.security
+    echo "Removed HSM security provider from java" && grep SunPKCS11 /opt/java/openjdk/conf/security/java_bc.security
 fi
 
 # -------- HSM -----------
@@ -20,9 +20,10 @@ fi
 # HSM_CERTIFICATE_SERIAL_NUMBER_HEX==Serial number of certificate in HSM in hex format
 SIGNMODULE_SERVICE_FILE_HSM=/etc/config/eidas-proxy/SignModule_Service_HSM_P12.xml
 if [ -f "$SIGNMODULE_SERVICE_FILE_HSM" ]; then
-    echo "Update keystore-config in $SIGNMODULE_SERVICE_FILE_HSM" && printenv | grep HSM_ALIAS
+    echo "Update keystore-config in $SIGNMODULE_SERVICE_FILE_HSM" && printenv | grep -E 'HSM_ALIAS|HSM_CERTIFICATE_SERIAL_NUMBER_HEX'
     sed -i "s/TRUSTSTORE_PASSWORD/$TRUSTSTORE_PASSWORD/g" $SIGNMODULE_SERVICE_FILE_HSM
-    sed -i "s/KEYSTORE_PASSWORD/$KEYSTORE_PASSWORD/g" $SIGNMODULE_SERVICE_FILE_HSM
+    sed -i "s/HSM_PASSWORD/$HSM_PASSWORD/g" $SIGNMODULE_SERVICE_FILE_HSM
     sed -i "s/HSM_ALIAS/$HSM_ALIAS/g" $SIGNMODULE_SERVICE_FILE_HSM
     sed -i "s/HSM_CERTIFICATE_SERIAL_NUMBER_HEX/$HSM_CERTIFICATE_SERIAL_NUMBER_HEX/g" $SIGNMODULE_SERVICE_FILE_HSM
+    sed -i "s/HSM_CERTIFICATE_ISSUER/$HSM_CERTIFICATE_ISSUER/g" $SIGNMODULE_CONNECTOR_FILE_HSM
 fi
